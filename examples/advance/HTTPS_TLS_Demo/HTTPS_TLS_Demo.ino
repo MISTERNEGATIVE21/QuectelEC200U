363d9fb8addc576b71ef104391bb93adadbe107b
MISTER_NEGATIVE <supergogetavegito21@gmail.com>
2025-11-14 03:00:35 +0530

    fix: Fix compilation errors in examples and ca_cert.h

--- examples/advance/HTTPS_TLS_Demo/HTTPS_TLS_Demo.ino
+++ examples/advance/HTTPS_TLS_Demo/HTTPS_TLS_Demo.ino
@@ -5,31 +5,34 @@
 #define EC200U_RX_PIN 16
 #define EC200U_TX_PIN 17
 
+QuectelEC200U* modem;
+
 #if defined(ARDUINO_ARCH_ESP32)
-HardwareSerial SerialAT(1);
-QuectelEC200U modem(SerialAT, 115200, EC200U_RX_PIN, EC200U_TX_PIN);
+HardwareSerial& SerialAT = Serial1;
 #else
 #include <SoftwareSerial.h>
 SoftwareSerial SerialAT(7, 8);
-QuectelEC200U modem(SerialAT);
 #endif
 
 const char* cert_path = "cloudflare_ca.pem";
 
 void setup() {
   Serial.begin(115200);
-#if !defined(ARDUINO_ARCH_ESP32)
+#if defined(ARDUINO_ARCH_ESP32)
+  modem = new QuectelEC200U(SerialAT, 115200, EC200U_RX_PIN, EC200U_TX_PIN);
+#else
+  modem = new QuectelEC200U(SerialAT);
   SerialAT.begin(9600);
 #endif
-  modem.begin();
+  modem->begin();
 
   // Set your APN here
-  modem.attachData("your.apn");
-  modem.activatePDP(1);
+  modem->attachData("your.apn");
+  modem->activatePDP(1);
 
   // Upload the Cloudflare CA certificate to the module's filesystem
   Serial.println("Uploading Cloudflare CA certificate...");
-  if (modem.fsUpload(cert_path, cloudflare_ca_cert)) {
+  if (modem->fsUpload(cert_path, cloudflare_ca_cert)) {
     Serial.println("Certificate uploaded.");
   } else {
     Serial.println("Failed to upload certificate.");
@@ -37,7 +40,7 @@ void setup() {
 
   // Before making an HTTPS request, you must configure the SSL context.
   // This includes setting the path to your CA certificate on the module's filesystem.
-  if (modem.sslConfigure(1, cert_path)) {
+  if (modem->sslConfigure(1, cert_path)) {
     Serial.println("SSL context configured.");
   } else {
     Serial.println("Failed to configure SSL context.");
@@ -45,7 +48,7 @@ void setup() {
 
   String resp;
   Serial.println("HTTPS GET from Cloudflare...");
-  if (modem.httpsGet("https://www.cloudflare.com/", resp)) {
+  if (modem->httpsGet("https://www.cloudflare.com/", resp)) {
     Serial.println("OK");
     Serial.println(resp);
   } else {
